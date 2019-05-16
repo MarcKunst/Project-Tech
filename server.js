a0de32519a5e24a721a4f6fe77fcd973d156134c
MarcKunst <marckunst93@gmail.com>
2019-05-16 11:25:16 +0200

    added file structure

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello Me!'))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/about', (req, res) => res.send('This is the about page'))
+
+app.get('/login', (req, res) => res.send('This is were you can log in'))
+// 404
+app.use(function (req, res, next) {
+    res.status(404).send("404 Sorry can't find that!")
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
