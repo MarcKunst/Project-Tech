a5fdd309446091621af4adc5818363da5b489633
MarcKunst <marckunst93@gmail.com>
2019-06-03 17:48:30 +0200

    fixed view routing

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const camelCase = require('camelcase');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+let upload = multer()
 
 
 const app = express()
 const port = 3000
 
 
-app.use(express.static('static')) //middleware
-app.set('view engine', 'ejs'); //devine templating engine
+//bodyparser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//static path
+app.use(express.static(path.join(__dirname, 'static')))
+
+//devine templating engine and path
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'view/pages'));
+
+
+//routes
+app.get('/', function(req, res) {
+  res.render('index')//route to index.ejs
+})
 
 app.get('/proposal_1.html', function(req, res) {
   res.sendFile(proposal_1.html)//route to html
@@ -24,3 +41,8 @@ app.use(function (req, res, next) {
   })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//form
+app.post('/profile', upload.none(), function (req, res, next) {
+  // req.body contains the text fields
+})
