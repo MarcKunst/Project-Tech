426acdf5a34bd5df87e703297e6940d43ae72226
MarcKunst <marckunst93@gmail.com>
2019-06-12 10:38:50 +0200

    starting with connecting to database

--- models/user.js
+++ models/user.js
@@ -1,24 +1,50 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: String,
-    password: String,
+
+    email: {
+        type: String,
+        lowercase: true,
+        index: true,
+        unique: true,
+        required: true
+    },
+    
+    password: {
+        type: String,
+        required: true
+    },
+
     name: {
         firstName: {
             type: String,
+            lowercase: true,
+            index: true,
             required: true
         },
-        lastName: String
+        lastName: {
+            type: String,
+            lowercase: true,
+            index: true
+        },
     },
+
     age: String,
+
     gender: String,
+
     profilePicture: Buffer,
+
     created: {
         type: Date,
         default: Date.now
-    }
+    },
 });
+
+userSchema.plugin(uniqueValidator);
+
 
 const User = mongoose.model('User', userSchema);
  
--- server.js
+++ server.js
@@ -51,11 +51,20 @@ app.use(expressValidator({
   }
 }));
 
-//mongoose
-mongoose.connect('mongodb://localhost/daterequest', function (err) {
-  if (err) throw err;
 
-});
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://localhost/daterequest';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
 
 //routes
 app.post('/', function(req, res) {
